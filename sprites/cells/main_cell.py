cb1ca3899dc3b3ad9c41708b944287c83ad08912
leeseongmin451 <leeseongmin451@gmail.com>
2022-06-18 08:53:05 +0900

    Apply `tetration_log10` function to MainCell

--- sprites/cells/main_cell.py
+++ sprites/cells/main_cell.py
@@ -1,4 +1,5 @@
 from .counting_cell import *
+from utilities import tetration_log10
 
 
 class MainCell(CountingCell):
@@ -20,4 +21,24 @@ class MainCell(CountingCell):
         self.rect = self.image.get_rect(center=(round(self.screen_pos[0]), round(self.screen_pos[1])))
 
         self.remove_text(0)
-        self.add_text("{}", (self.number, ), "fonts/Quicksand-Bold.ttf", 30, "center", (self.width // 2, self.height // 2))
+        self.add_text("{}", (self.number, ), "fonts/Quicksand-Bold.ttf", 30, "center", (self.width // 2, self.height // 2 - 20))
+        self.add_text("= 10^^{}", (round(tetration_log10(self.number), 4), ), "fonts/Quicksand-Bold.ttf", 30, "center", (self.width // 2, self.height // 2 + 20))
+
+    def update(self):
+        """
+        Updates this sprite every frame
+
+        :return: None
+        """
+
+        mouse_state = Player.mouse
+
+        self.update_text_args(0, (round(self.number, ROUNDING_DIGITS), ))
+        self.update_text_args(1, (round(tetration_log10(self.number), 4), ))
+        Button.update(self)
+
+        self.screen_pos = trans_field_to_screen(self.field_pos)
+        self.rect.center = (round(self.screen_pos[0]), round(self.screen_pos[1]))
+
+        if self.selected and not mouse_state.left.clicked:
+            self.selected = False
